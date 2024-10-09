f72c27ed1843b4a0f7f85d6b417ea00ab08e9919
Web-experiment <johnwillis.alande@strathmore.edu>
2024-10-09 23:17:06 +0300

    2.5-Filters

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -11,6 +11,43 @@ fun main(args: Array<String>) {
     swim()
     swim("slow")
     swim(speed = "turtle-like")
+//    println(decorations.filter {it[0] == 'p'})
+
+    //eager, creates a new list
+    val eager = decorations.filter {it[0] == 'p'}
+    println("eager: $eager")
+
+    //lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter {it[0] == 'p'}
+    println("filtered: $filtered")
+
+    //force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 }
 
 fun feedTheFish() {
@@ -54,3 +91,5 @@ fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boo
 fun isTooHot(temperature: Int) = temperature > 30
 fun isDirty(dirty: Int) = dirty > 30
 fun isSunday(day: String) = day == "Sunday"
+
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
