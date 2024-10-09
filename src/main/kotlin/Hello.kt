091789fa542386b99266aa3c51234a8bed6dd20b
Web-experiment <johnwillis.alande@strathmore.edu>
2024-10-09 21:58:25 +0300

    Learning value in Kotlin

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello, world:::)")
+    val temperature = 80
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"}."
+    println(message)
 }
