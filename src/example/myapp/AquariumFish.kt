632c65fee86314a7757b13067223031bb57b46d4
Web-experiment <johnwillis.alande@strathmore.edu>
2024-11-04 11:17:46 +0300

    3.7-User Interface Delegation

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -1,23 +1,36 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+//abstract class AquariumFish {
+//    abstract val color: String
+//}
+
+interface FishAction {
+    fun eat()
 }
 
-class Shark: AquariumFish(), FishAction {
+interface FishColor {
+    val color: String
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("Hunt and Eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
-    override fun eat() {
-        println("Eat Algae")
-    }
-}
+// user interface delegation
+class Plecostomus(fishColor: FishColor = GoldColor):
+        FishAction by PrintingFishAction("eat algae"),
+        FishColor by fishColor
 
-interface FishAction {
-    fun eat()
-}
