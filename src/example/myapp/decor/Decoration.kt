90bda31d49b5e7c59045591b5a2794c839e2aa2e
Web-experiment <johnwillis.alande@strathmore.edu>
2024-11-04 11:35:36 +0300

    3.9-Learn about singletons and enums

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -33,6 +33,16 @@ fun makeDecorations() {
 }
 
 
+//fun main() {
+//    makeDecorations()
+//}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun main() {
-    makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
